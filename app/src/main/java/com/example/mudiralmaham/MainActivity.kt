ff2c42fb02583211e0a271b62ff5c209f274a4c3
sajad rezvani <majidstic@yahoo.com>
2019-06-24 14:32:12 +0430

    added base login fragment

--- app/src/main/java/com/example/mudiralmaham/MainActivity.kt
+++ app/src/main/java/com/example/mudiralmaham/MainActivity.kt
@@ -3,10 +3,20 @@ package com.example.mudiralmaham
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import com.example.mudiralmaham.pages.LoginFragment
 
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         setContentView(R.layout.main_activity)
+        showLoginFragment()
+    }
+
+    private fun showLoginFragment() {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        val loginFragment = LoginFragment()
+        fragmentTransaction.add(R.id.fragment_holder, loginFragment)
+        fragmentTransaction.commit()
+
     }
 
     override fun onDestroy() {
