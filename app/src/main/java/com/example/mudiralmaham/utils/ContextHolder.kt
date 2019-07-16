d9def293726a9e959547e64879df03169aaa4b34
aafsari <amirafsari99@yahoo.com>
2019-07-16 18:06:27 +0430

    fake data

--- app/src/main/java/com/example/mudiralmaham/utils/ContextHolder.kt
+++ app/src/main/java/com/example/mudiralmaham/utils/ContextHolder.kt
@@ -3,6 +3,7 @@ package com.example.mudiralmaham.utils
 import android.annotation.SuppressLint
 import com.example.mudiralmaham.dataModels.*
 import com.example.mudiralmaham.webservice.EndPoints
+import java.util.*
 
 object ContextHolder {
     val databaseName: String = "makhzan"
@@ -15,12 +16,42 @@ object ContextHolder {
     lateinit var tasks: MutableList<Task>
     var user: User? = null
 
-    fun getCacheData() {
+    fun getCacheData() {/*
         tasks = Database.getTasks()
-        projects = Database.getProjects()
+        projects = Database.getProjects()*/
+        tasks = mutableListOf()
+        projects = mutableListOf()
+
+        for (i in 1..10) {
+            var p = Project()
+            p.name = "Project Number $i"
+            p.created_date = Date()
+            projects.add(p)
+        }
+
+        for (i in 1..10) {
+            var t = Task()
+            t.name = "Task Number $i"
+            t.created_date = Date()
+            t.due_date = Date()
+            t.owner = "me"
+            t.project = "Project Number 1"
+            tasks.add(t)
+        }
     }
 
     fun getProjectTasks(projectName: String): MutableList<Task> {
-        return Database.getProjectTasks(projectName)
+//        return Database.getProjectTasks(projectName)
+        val taskList = mutableListOf<Task>()
+        for (i in 1..10) {
+            var t = Task()
+            t.name = "Task Number $i from $projectName "
+            t.created_date = Date()
+            t.due_date = Date()
+            t.owner = "me"
+            t.project = "Project Number 1"
+            taskList.add(t)
+        }
+        return taskList
     }
 }
